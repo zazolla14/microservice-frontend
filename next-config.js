60abda3f8d0da26a6afe77ecb51d1d0aca170ad3
Azola Zubizarreta <azola.zubizarreta@gmail.com>
2021-01-10 02:00:11 +0700

    first initial

---
+++ next-config.js
@@ -0,0 +1,15 @@
+const withPlugins = require('next-compose-plugins')
+const withReactSvg = require('next-react-svg')
+const withImages = require('next-images')
+
+const path = require('path')
+
+module.exports = withPlugins([
+  withImages({}),
+  withReactSvg({
+    include: path.resolve(__dirname, './public/images'),
+    webpack(config, options) {
+      return config
+    },
+  }),
+])
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
